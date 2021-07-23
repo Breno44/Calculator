f114c24b4d927ab83d094da461717d8cadb276fc
Breno Henrique <bhfs.2908@gmail.com>
2021-07-23 15:46:08 -0300

    add button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Button.css'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default props => 
     <button className="button">{props.label}</button>
