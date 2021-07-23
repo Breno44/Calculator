33a81f5dc54e2517f07b95301f5efd8127368a76
Breno Henrique <bhfs.2908@gmail.com>
2021-07-23 15:39:41 -0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import './Button.css'
+
+export default props => 
+    <button className="button">{props.label}</button>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator'; 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
